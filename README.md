27cb61db4b38c061b6a553d007e4b758baf801fa
Aseem Wangoo <aseemwangoo@gmail.com>
2021-04-05 14:12:35 +0800

    [C/Init] Proj commit

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# dynamism
+# screener
+- This is a sample application.
+
+### Getting Started 🎯🎯
+
+- Download the repo
+- Make sure you are on the Flutter Stable Channel
+
+```dart
+flutter channel stable
+flutter upgrade
+
+```
+
+- Install dependencies, when asked, after downloading the project.
+- Run the cmd 
+
+```dart
+flutter run
+
+```
+
+- You should see the app working at this point..
---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(MainApp());
+}
+
+class MainApp extends StatefulWidget {
+  @override
+  _MainAppState createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Screener',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: const _Home(),
+    );
+  }
+}
+
+class _Home extends StatelessWidget {
+  const _Home({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Screener'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
